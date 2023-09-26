051e04f98e6146ade6b0ad267307df0df952cab1
unknown <dewibelajar10@gmail.com>
2023-09-26 21:18:27 +0700

    Tugas jurnal 1

---
+++ JurnalSatu.java
@@ -0,0 +1,11 @@
+public class JurnalSatu {
+    public static void main(String[] args){
+		String[] tugas = {"Dewi Puspita Sari", "607062300020", "D3IF-47-03"};
+		String nim = "607062300020";
+		String kelas = "D3IF-47-03";
+		
+		System.out.println("Nama :" + tugas[0]);
+		System.out.println("NIM : " + tugas[1]);
+		System.out.println("Kelas : " + tugas[2]);
+	}
+}
